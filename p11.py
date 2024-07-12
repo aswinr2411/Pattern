d5ebd6b8be914d3229d51d5a584d2e382fa65c67
aswinr2411 <151996041+aswinr2411@users.noreply.github.com>
2024-07-12 21:32:48 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -1,4 +1,5 @@
 def drawl(row,co):
+    print(row,co)
     for i in range(0,co):
         if(i%2==0):
             print(" ___","   ",end="")
@@ -15,5 +16,7 @@ def drawl(row,co):
         print(end="\n")
         for z in range(row):
             print("\\" + "___" + "/" + "   ", end="")
+    print("\n")
 
 drawl(3,5)
+drawl(4,7)
